8b6a80e25d65b22e4b2f1792e98e938b31ead472
Your Name <yong@Xues-MacBook-Pro.local>
2014-03-29 00:05:06 -0400

    update init

--- app/js/application.js
+++ app/js/application.js
@@ -2,6 +2,11 @@ animationDelay = 100;
 minSearchTime = 100;
 
 // Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
-});
+window.onload = function() {
+	OrangeeJS.init(function() {
+    window.requestAnimationFrame(function () {
+      var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+    });
+	});
+
+};
